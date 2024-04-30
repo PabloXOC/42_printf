713cb218ce2a870d82be7c71c22c9f4f12f514a8
Pablo <paxoc01@gmail.com>
2024-04-30 16:40:16 +0200

    uploaded all

---
+++ get_next_line.c
@@ -0,0 +1,155 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pximenez <pximenez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 15:49:13 by pximenez          #+#    #+#             */
+/*   Updated: 2024/01/12 12:37:35 by pximenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_save_rest_static(char *static_var_in, char *line)
+{
+	int		i_0;
+	int		i_f;
+	char	*static_var_out;
+
+	if (static_var_in == NULL)
+		return (NULL);
+	i_f = ft_strlen(static_var_in);
+	i_0 = ft_strlen(line);
+	static_var_out = (char *) malloc ((i_f - i_0 + 1) * sizeof(char));
+	if (!(static_var_out))
+	{
+		free(static_var_in);
+		return (NULL);
+	}
+	i_f = 0;
+	while (static_var_in[i_0 + i_f] != 0)
+	{
+		static_var_out[i_f] = static_var_in[i_f + i_0];
+		i_f++;
+	}
+	static_var_out[i_f] = 0;
+	free(static_var_in);
+	return (static_var_out);
+}
+
+char	*ft_save_line(char *static_var_in)
+{
+	char	*line_out;
+	int		i_0;
+	int		i_f;
+
+	if (static_var_in == NULL)
+		return (NULL);
+	i_0 = 0;
+	i_f = 0;
+	while (static_var_in[i_f] != '\n' && static_var_in[i_f] != 0)
+		i_f++;
+	if (static_var_in[i_f] == '\n')
+		i_f++;
+	line_out = (char *) malloc ((i_f + 1) * sizeof(char));
+	if (!(line_out))
+		return (NULL);
+	while (i_0 < i_f)
+	{
+		line_out[i_0] = static_var_in[i_0];
+		i_0++;
+	}
+	line_out[i_0] = 0;
+	return (line_out);
+}
+
+char	*ft_add_to_static_2(int bytes_read, char *buf, char *static_var_in)
+{
+	if (bytes_read == -1)
+	{
+		if (static_var_in != NULL)
+			free(static_var_in);
+		if (buf != NULL)
+		{
+			buf[0] = 0;
+			free(buf);
+		}
+		return (NULL);
+	}
+	buf[bytes_read] = 0;
+	if (!static_var_in)
+	{
+		if (buf[0] != '\0')
+			return (buf);
+		free (buf);
+		return (NULL);
+	}
+	if (ft_end(buf, static_var_in) == 0)
+		return (NULL);
+	static_var_in = ft_strjoin(static_var_in, buf);
+	return (static_var_in);
+}
+
+char	*ft_add_to_static(int fd, char *static_var_in)
+{
+	int		bytes_read;
+	char	*buf;
+
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		buf = (char *) malloc (BUFFER_SIZE + 1);
+		if (!buf)
+		{
+			if (static_var_in != NULL)
+				free(static_var_in);
+			return (NULL);
+		}
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		static_var_in = ft_add_to_static_2(bytes_read, buf, static_var_in);
+		if (static_var_in == NULL)
+			return (NULL);
+		if (ft_strchr(static_var_in, '\n') == 1)
+			return (static_var_in);
+	}
+	return (static_var_in);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*static_var;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (static_var != NULL)
+	{
+		if (ft_strchr(static_var, '\n') == 1)
+		{
+			line = ft_save_line(static_var);
+			static_var = ft_save_rest_static(static_var, line);
+			return (line);
+		}
+	}
+	static_var = ft_add_to_static(fd, static_var);
+	line = ft_save_line(static_var);
+	static_var = ft_save_rest_static(static_var, line);
+	return (line);
+}
+
+/* int	main(void)
+{
+	int	fd;
+	int	i;
+
+	i = 0;
+	fd = open("text_2.txt", O_RDONLY);
+	while (i < 1)
+	{
+		printf("%s", get_next_line(fd));
+		i++;
+	}
+	while(42);
+} */
---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pximenez <pximenez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 11:01:07 by pablo             #+#    #+#             */
+/*   Updated: 2024/01/12 12:39:20 by pximenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <ctype.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+int		ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*get_next_line(int fd);
+int		ft_end(char *buf, char *static_var);
+
+#endif
