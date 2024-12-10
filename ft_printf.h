270227e79e765de9ac5567a2470997f7b435031b
Pablo XOC <paxoc01@gmail.com>
2024-12-10 19:29:34 +0100

    added correct file

--- ft_printf.h
+++ ft_printf.h
@@ -1,36 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pximenez <pximenez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paxoc01 <paxoc01@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 11:01:07 by pablo             #+#    #+#             */
-/*   Updated: 2024/01/12 12:47:51 by pximenez         ###   ########.fr       */
+/*   Updated: 2024/01/25 18:25:53 by paxoc01          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+//libft
 # include <ctype.h>
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
+//printf
 # include <stdarg.h>
+# include <stdint.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
-# endif
-
-char	*get_next_line(int fd);
-int		ft_strchr(const char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_strlen(const char *s);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-char	*get_next_line(int fd);
-int		ft_end(char *buf, char *static_var);
+//funciones
+int	ft_printf(char const *fmt, ...);
+int	ft_c(char c, int len);
+int	ft_s(char *s, int len);
+int	ft_p(uintptr_t nb, int len, char *hexa);
+int	ft_di(int nb, int len);
+int	ft_u(unsigned int nb, int len);
+int	ft_xx(unsigned int nb, int len, char *hexa);
+int	ft_perc(int len);
 
 #endif
